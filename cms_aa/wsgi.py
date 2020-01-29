0dd8cf8fc5ac92f017e6abf7229bf9bfbd5408fb
ayeshadmian <41767121+ayeshadmian@users.noreply.github.com>
2020-01-29 10:42:41 -0600

    Update wsgi.py

--- cms_aa/wsgi.py
+++ cms_aa/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
+import sys
+DJANGO_PATH =  os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+sys.path.append(DJANGO_PATH)
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cms_aa.settings')
 
 application = get_wsgi_application()
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)

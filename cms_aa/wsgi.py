207c7d6e780c2f7c571927e9b528bdcf4e36f38a
ayeshadmian <41767121+ayeshadmian@users.noreply.github.com>
2020-01-29 11:25:29 -0600

    Update wsgi.py

--- cms_aa/wsgi.py
+++ cms_aa/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 import os
 #import sys
 #sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-os.environ['PYTHONPATH'] = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
+#os.environ['PYTHONPATH'] = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
 #DJANGO_PATH =  os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
 #sys.path.append(DJANGO_PATH)
 
